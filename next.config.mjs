4045f28680427047cc16b447a996096b93791370
LegzNepobedivi <139544404+LegzNepobedivi@users.noreply.github.com>
2024-07-19 21:31:48 +0200

    Accounts almost done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  //   reactStrictMode: true,
+  //   images: {
+  //     remotePatterns: [
+  //       {
+  //         protocol: "https",
+  //         hostname: "res.cloudinary.com",
+  //         pathname: "**",
+  //       },
+  //     ],
+  //   },
+  //   async headers() {
+  //     return [
+  //       {
+  //         source: "/(.*)",
+  //         headers: [
+  //           {
+  //             key: "X-Frame-Options",
+  //             value: "DENY",
+  //           },
+  //         ],
+  //       },
+  //     ];
+  //   },
+  //   experimental: {
+  //     typedRoutes: true,
+  //   },
+};
 
 export default nextConfig;
